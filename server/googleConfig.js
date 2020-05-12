65446d4bcf477b5be6962b9507b00803b3bc1e59
M1NDX <luisfer.isc@gmail.com>
2020-05-11 21:59:39 -0500

    Delete googleConfig.js

--- server/googleConfig.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    clientID: '681749206459-bpc30q9a7rsp7dbmulpqftgktumkljp0.apps.googleusercontent.com',
-    clientSecret: 'KmHknP3cEyOJtcfOga4_cGT2',
-    callbackURL: "http://localhost:3000/google/redirect"
-}
